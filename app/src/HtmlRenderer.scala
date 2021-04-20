6554119e6870f69a633358848b4a54972c7e6aeb
David Selassie <dsel@fastmail.com>
2021-04-20 15:46:04 -0700

    Abs paths to CSS

--- app/src/HtmlRenderer.scala
+++ app/src/HtmlRenderer.scala
@@ -14,8 +14,8 @@ object HtmlRenderer {
             name := "viewport",
             content := "width=device-width, initial-scale=1"
           ),
-          link(rel := "stylesheet", href := "static/site.css"),
-          link(rel := "icon", href := "static/favicon.png"),
+          link(rel := "stylesheet", href := "/static/site.css"),
+          link(rel := "icon", href := "/static/favicon.png"),
           title("Beerdledash"),
           headExtras
         ),
